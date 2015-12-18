e1424d923b5daa09f5eeedc9446375cf97c3ca0f
Hendra Susanto <benhanks040888@gmail.com>
2015-12-18 16:01:10 +0700

    save dulu

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var jade = require('gulp-jade');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.config.production = true;
+
+var outputDir = 'public/assets/',
+    jsDir = outputDir + 'js/',
+    cssDir = outputDir + 'css/';
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+  mix.task('jade', 'public/html/jade/**/*.jade');
+
+  mix
+    .browserSync({
+      open: false,
+      files: [cssDir + '**/*.css', 'public/html/**/*.html'],
+      proxy: 'http://makasih.dev',
+    })
+    .sass('app.scss', cssDir)
+    .scripts(['plugins.js', 'app.js'], jsDir + 'app.js');
+});
+
+gulp.task('jade', function() {
+  return gulp.src(['public/html/jade/**/*.jade', '!public/html/jade/**/layout.jade', '!public/html/jade/**/_includes/*.jade'], {
+      base: 'public/html/jade/pages'
+    })
+      .pipe(jade({
+        pretty: true
+      }))
+      .pipe(gulp.dest('public/html/'))
 });
